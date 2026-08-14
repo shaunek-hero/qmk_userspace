71ae74ca55c4743c79d63572375da49a18bcaeb0
Shaun Ek <shaun@herobullion.com>
2026-08-14 15:13:18 -0600

    Remove number layer on G

--- keyboards/beekeeb/piantor/keymaps/shaun/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/shaun/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,
                                                                                  KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,            KC_BSPC,
-        KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), LT(_NUM, KC_G),
+        KC_ESC,  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,
                                                                                  KC_H,         RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
         KC_UNDS, ALL_T(KC_Z),  KC_X,         KC_C,         KC_V,         KC_B,
                                                                                  KC_N,         KC_M,         KC_COMM,      KC_DOT,       ALL_T(KC_SLSH),  KC_MINS,
